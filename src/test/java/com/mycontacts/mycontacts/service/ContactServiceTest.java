6d5d2c98a9a85c069730c63b6a2558ce59d3e0d8
Helena <helena.slva19@gmail.com>
2026-05-11 19:16:14 -0300

    test: blocking commits teste

--- src/test/java/com/mycontacts/mycontacts/service/ContactServiceTest.java
+++ src/test/java/com/mycontacts/mycontacts/service/ContactServiceTest.java
@@ -49,7 +49,7 @@ class ContactServiceTest {
 
         List<Contact> result = service.findAll();
 
-        assertThat(result).hasSize(3);
+        assertThat(result).hasSize(4);
         assertThat(result).hasSize(2);
         assertThat(result).extracting(Contact::getName).containsExactly("Maria", "Pedro");
     }

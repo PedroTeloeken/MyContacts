a1084d31359f3358fb2b2e7cbf15651cfe5e7bcb
Helena <helena.slva19@gmail.com>
2026-05-13 19:31:49 -0300

    chore: remove unused  imports

--- src/main/java/com/mycontacts/mycontacts/service/ContactService.java
+++ src/main/java/com/mycontacts/mycontacts/service/ContactService.java
@@ -3,11 +3,7 @@ package com.mycontacts.mycontacts.service;
 import com.mycontacts.mycontacts.entity.Contact;
 import com.mycontacts.mycontacts.repository.ContactRepository;
 import java.util.List;
-import java.util.Optional;
-
-import org.hibernate.annotations.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
